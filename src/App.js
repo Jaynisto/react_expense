7cd398bd61268e9d558d218acdbda5571517e539
Fanie <faniejohnsons98@gmail.com>
2023-09-05 09:57:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import ExpenseItem from './components/ExpenseItem'
+
+function App() {
+  const expenses = [
+    {
+      id:'e1',
+      title:'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020,7,14)
+    },
+    {
+      id:'e2',
+      title:'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021,2,28)
+    },
+    {
+      id:'e3',
+      title:'Cosmetics',
+      amount: 114.12,
+      date: new Date(2023,7,14)
+    }
+  ]
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem 
+      expense={expenses[0]}
+      ></ExpenseItem>
+      <ExpenseItem 
+      expense={expenses[1]}
+      ></ExpenseItem>
+      <ExpenseItem 
+      expense={expenses[2]}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import './ExpenseItem.css'
+function ExpenseItem(props){
+    const month = props.expense.date.toLocaleString('en-US', { month: 'long'});
+    const day = props.expense.date.toLocaleString('en-US', { day: '2-digit'});
+    const year = props.expense.date.getFullYear();
+    return (
+        <div className="expense-item">
+            <div>{day}</div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div className="expense-item__description">
+                <h2>{props.expense.title}</h2>
+                <div className="expense-item__price">R{props.expense.amount}</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
